6710d02f5527a1ea505cb41c880c87e9727fc2e7
Mustafa <MustafaGino1@yahoo.com>
2022-11-20 20:01:56 -0600

    finished part 2, last commit.

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,10 +1,9 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
-
 
 // 0. WRITE YOUR DESCRIPTION HERE
 // //EXAMPLE. We make a variable called number and set it to 1 as a number.
@@ -23,7 +22,6 @@ if (number >= 2) {
 }
 
 */
-
 
 // 1. WRITE YOUR DESCRIPTION HERE
 // We created a variable called 'name' and assign it to 'Dane'
@@ -49,7 +47,13 @@ if (name === 'Mary') {
 // we assigned 'code' the value of the numbers 1,2, and 3.
 // Here, were writing a conditional stating if 'code' and the numbers 123
 // are equal in value and type.
-//
+// Secret is assigned value of 'super'
+// code is assigned to the value of 'code' which we previously gave the value of the numbers 123, multiplied by the number 2.
+//another conditional stating if code is greater than or equal to 250.
+// we assigned the variable 'secret' the value of 'duper'
+// we console.log(secret)
+
+
 
 //CODE
 /*
@@ -104,11 +108,8 @@ if (isStudent === true && zip > 80000 ) {
 
 */
 
-
-
-
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -117,7 +118,7 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
@@ -132,15 +133,17 @@ if (number >= 2) {
   console.log('yes');
 }
 
-*/
+* 
 
-
-
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+
+//FIX-- instructions asked to set colorOne to blue but its set to red and vice versa.
+// it should be colorOne = 'blue'; colorTwo = 'blue'; 
+
 let colorOne = 'red';
 let colorTwo = 'blue';
 let mix = true;
@@ -151,30 +154,34 @@ if (mix === true) {
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
 let temp = 40;
 const time = 4;
 
+//-- FIX "||" translates to "or". solution: replace the "||" with "&&". if (temp > 39 && time >= 4)
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
+// 
 // -- it is, so we console.log 'enter'.
 
 /*
 let age = 21;
 const minAge = 21;
 
+//FIX-- instructions asked to check if age is greater than or equal to miniAge. it should be (Age >= miniAge).
+2nd FIX-- instructions stated to console.log "enter" first but its the second console log being ran. console.Log('enter');
 if(minAge <= age) {
   console.log('no entry');
 } else {
   console.log('enter');
 }
 */
-
